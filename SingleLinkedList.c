d7685b1d03bc4fe58ea4bb07997df37ef3f13e19
Sonu Lodhi <sp95759201@gmail.com>
2024-12-15 23:27:30 +0530

    crate a InsertInLastt function

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -8,7 +8,7 @@ struct node{
 
 };
 struct node *head;
-
+/*function for to insert a value in the start of the list*/
 void InsertInStart(){
     struct node *ptr;
     int item;
@@ -26,9 +26,45 @@ void InsertInStart(){
     }
 }
 
+/*function for to insert a value in the last of the list*/
+void InsertInLast()
+{
+    struct node *ptr, *temp;
+    int item;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL)
+    {
+        printf("\nOVERFLOW");
+    }
+    else
+    {
+        printf("\nEnter value: ");
+        scanf("%d", &item);
+        ptr->data = item;
+        if (head == NULL)
+        {
+            ptr->next = NULL;
+            head = ptr;
+            printf("Item inserted");
+        }
+        else
+        {
+            temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = ptr;
+            ptr->next = NULL;
+            printf("Item inserted");
+        }
+    }
+}
+
 int main(){
 
     InsertInStart();
+    InsertInLast();
 
     return 0;
 }
