7800f5059647a0fbc6f83b118438a8a5f1c1a233
Sonu Lodhi <sp95759201@gmail.com>
2024-12-19 20:48:42 +0530

    complete the search function

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -178,6 +178,30 @@ void display(){
 }
 
 void search(){
+    struct node *ptr;
+    int item, i = 0, flag;
+    ptr = head;
+    if (ptr == NULL){
+        printf("\nEmpty List\n");
+    }
+    else{
+        printf("\nEnter searching item: ");
+        scanf("%d", &item);
+        while (ptr != NULL){
+            if (ptr->data == item){
+                printf("Item found at location %d ", i + 1);
+                flag = 0;
+            }
+            else{
+                flag = 1;
+            }
+            i++;
+            ptr = ptr->next;
+        }
+        if (flag == 1){
+            printf("Item not found\n");
+        }
+    }
 
 }
 
