7a31a5c30de397c67a7d89d3d098e9ffc4c1e2a1
Sonu Lodhi <sp95759201@gmail.com>
2025-01-26 17:10:05 +0530

    add insert at head function

--- singlelinkedlist2.c
+++ singlelinkedlist2.c
@@ -1,24 +1,27 @@
-#include<stdio.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef int item_type;
 
-
-typedef struct  n_tag{
+typedef struct n_tag
+{
     item_type data;
     struct n_tag *next;
-}node;
+} node;
 
-typedef struct l_tag{
+typedef struct l_tag
+{
     node *start;
     node *end;
 
-}list;
+} list;
 
-node *create_node(item_type x){
+node *create_node(item_type x)
+{
     node *p;
-    p = (node*)malloc(sizeof(node));
-    if(p==NULL){
+    p = (node *)malloc(sizeof(node));
+    if (p == NULL)
+    {
         printf("memory does not avilabel");
     }
 
@@ -28,34 +31,55 @@ node *create_node(item_type x){
     return p;
 }
 
-void intialize(list *lp){
+void intialize(list *lp)
+{
 
     lp->start = NULL;
     lp->end = NULL;
-
 }
 
-void display(list *lp){
+void display(list *lp)
+{
 
     node *p;
     p = lp->start;
 
     while (p != NULL)
     {
-        printf('%d /n', p->data);
+        printf("%d \n", p->data);
 
         p = p->next;
     }
-    
-
 }
 
-
-
-
-int main(){
+void insert_head(list *lp, item_type x){
+    node *newp;
+    newp = create_node(x);
     
+    if(lp->start == NULL){
+        lp->start = newp;
+        lp->end = newp;
+    }
+    else{
+    newp->next = lp->start;
+    lp->start = newp;   
+    }
+}
 
+int main()
+{
+    list *lp = (list *)malloc(sizeof(list)); // Allocate memory for the list
+    if (lp == NULL)
+    {
+        printf("Memory allocation for list failed\n");
+        return 1;
+    }
+    intialize(lp);
+    insert_head(lp, 55);
+    insert_head(lp, 2);
+    insert_head(lp, 3);
+    insert_head(lp, 4);
+    display(lp);
 
     return 0;
 }
